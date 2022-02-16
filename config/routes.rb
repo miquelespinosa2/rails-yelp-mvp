bb7d8be07f6d019b6321bc82e233d7bbad1b4bee
Miquel Espinosa <miquelespinosa2@gmail.com>
2022-02-16 16:23:09 +0100

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+    # /restaurants/id/reviews/new
+  end
+
+  resources :reviews, only: [:destroy]
 end
